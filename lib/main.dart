22258930c83187b5489e8686f7605b69053abefc
muktaahammed <mukta192426@gmail.com>
2021-06-12 16:58:13 +0800

    sample code added

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Color Palette'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Colors"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Easy and Customizable Flutter color palette to use whole application\n" +
+              "To get the code see in the const folder",
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
